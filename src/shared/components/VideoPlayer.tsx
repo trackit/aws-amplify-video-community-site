e268ce1c32c1d3af9d4742c6d3ae7801522ec053
Nathan AGEZ <nathan.agez@epitech.eu>
2021-04-27 16:26:58 +0200

    fix(video-player): Fix types creating CORS issues

--- src/shared/components/VideoPlayer.tsx
+++ src/shared/components/VideoPlayer.tsx
@@ -1,61 +1,56 @@
-import React from 'react';
-import 'video.js/dist/video-js.css';
-import './VideoPlayer.css'
-import videojs from 'video.js';
+import React from "react";
+import "video.js/dist/video-js.css";
+import "./VideoPlayer.css";
+import videojs from "video.js";
 
 interface VideoPlayerPropsInferface extends videojs.PlayerOptions {
-    videoJsOptions: videojs.PlayerOptions;
-    sources: Array<{src: string, type: string}>
+    token: string;
 }
 
-export default class VideoPlayer extends React.Component<any> {
-    private player?: videojs.Player;
-    private videoNode?: HTMLVideoElement;
-    private options?: videojs.PlayerOptions
+export default class VideoPlayer extends React.Component<VideoPlayerPropsInferface> {
+  private player?: videojs.Player;
+  private videoNode?: HTMLVideoElement;
+  private options?: VideoPlayerPropsInferface;
 
-    constructor(props: videojs.PlayerOptions) {
-        super(props);
-        this.options = props;
-        this.player = undefined;
-        this.videoNode = undefined;
-    }
+  constructor(props: VideoPlayerPropsInferface) {
+    super(props);
+    this.options = props;
+    this.player = undefined;
+    this.videoNode = undefined;
+  }
 
-    componentDidMount() {
-        const { src } = this.props.sources[0]
-        console.log("src:", this.props.token)
-        //  instantiate video.js
-        this.player = videojs(this.videoNode, this.options).ready(function() {
-            console.log('onPlayerReady', this);
-        });
-        videojs.xhr({
-            url: src,
-            beforeSend: function (options: any) {
-                options.url = `${options.url}${videojs.getAllPlayers()[0].options(null).token}`;
-                console.log(options)
-                return options;
-            }
-        }, (res) => {
-            console.log('Player res: ', res);
-        });
-    }
+  componentDidMount() {
+    // @ts-ignore
+    videojs.Vhs.xhr.beforeRequest = (
+      options: any
+    ) => {
+      options.uri = `${options.uri}${this.options?.token}`
+      return options;
+    };
 
-    // destroy player on unmount
-    componentWillUnmount() {
-        if (this.player) {
-            this.player.dispose();
-        }
-    }
+    this.player = videojs(this.videoNode, this.options).ready(function () {
+      console.log("onPlayerReady", this);
+    });
+  }
 
-    // wrap the player in a div with a `data-vjs-player` attribute
-    // so videojs won't create additional wrapper in the DOM
-    // see https://github.com/videojs/video.js/pull/3856
-    render() {
-        return (
-            <div className='video-player'>
-                <div data-vjs-player>
-                    <video ref={(node: HTMLVideoElement) => { this.videoNode = node; }} className="vjs-fluid video-js vjs-default-skin vjs-big-play-centered"/>
-                </div>
-            </div>
-        );
+  componentWillUnmount() {
+    if (this.player) {
+      this.player.dispose();
     }
+  }
+
+  render() {
+    return (
+      <div className="video-player">
+        <div data-vjs-player>
+          <video
+            ref={(node: HTMLVideoElement) => {
+              this.videoNode = node;
+            }}
+            className="vjs-fluid video-js vjs-default-skin vjs-big-play-centered"
+          />
+        </div>
+      </div>
+    );
+  }
 }
